4260f69c5ab8558b526267843124b4847775371f
swapnilz30 <swapnilzarbade@gmail.com>
2017-06-01 02:05:42 +0530

    struct_menu.c added

--- struct_menu.c
+++ struct_menu.c
@@ -13,30 +13,31 @@ void main()
 	printf("1. First\n");
 	printf("2. Second\n");
 	printf("3. Third\n");
-	scanf("%d",&m1.num);
 	do
 	{
+		scanf("%d",&m1.num);
+		switch(m1.num)
+		{
+			case 1:
+				printf("You have entered 1st option in the menu.\n");
+				break;
+			case 2:
+				printf("You have entered 2nd option in the menu.\n");
+				break;
+			case 3:
+				printf("You have entered 3rd` option in the menu.\n");
+				break;
+			case 4:
+				exit(0);
+				break;
+			default: 
+				break;
+		}
 		printf("1. First\n");
 		printf("2. Second\n");
 		printf("3. Third\n");
-		printf("4. Enter four for exit");
-		if(m1.num==1)
-		{
-			printf("You have entered 1st option in the menu.");
-		}
-		if(m1.num==2)
-		{
-			printf("You have entered second option in the menu.");
-		}
-		if(m1.num==3)
-		{
-			printf("You have entered 3rd option in the menu.");
-		}
-		if(m1.num==4)
-		{
-			exit(0);
-		}
-		getch();
+		printf("4. Enter four for exit\n");
+		getchar();
 
 	} while(1); 
 }	
