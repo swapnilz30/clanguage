552dd69d815a5ef70257ce3d7d102a9ce578bf1f
swapnilz30 <swapnilzarbade@gmail.com>
2017-06-04 05:11:05 +0530

    fix with functions struct_bank.c

--- struct_bank.c
+++ struct_bank.c
@@ -10,16 +10,16 @@ struct bank
 };
 
 void menu();
-void check_balance();
-void deposit(struct bank b);
-void withdraw(struct bank b);
+void check_balance(struct bank b);
+void deposite(struct bank b);
+void withdrawal(struct bank b);
 
 void main()
 {
 
 	struct bank b;
+
 	printf("Welcome to ABC bank.\n");
-        menu();	
 
 	printf("Enter account no: ");
 	scanf("%d",&b.accountno);
@@ -27,8 +27,29 @@ void main()
 	switch(b.accountno)
 	{
 		case 1:
-			b.balance = 10000;
-			check_balance(b);
+			b.balance = 10000.00;
+			menu();
+			scanf(" %c", &b.trans);
+			switch(b.trans)
+			{
+				case 'b':
+					check_balance(b);
+					break;
+				case 'd':
+					printf("Enter the amount for deposite.\n");
+					scanf("%f",&b.amount);
+					deposite(b);
+					break;
+				case 'w':
+					printf("Enter the amount to withdrawal.\n");
+					scanf("%f",&b.amount);
+					withdrawal(b);
+					break;
+				default:
+					printf("Enter correct option for transaction.\n");
+					break;
+			}
+
 			break;
 		default:
 			printf("Enter correct account number\n");
@@ -39,7 +60,6 @@ void main()
 
 void menu()
 {
-	printf("Enter your account no.\n");
 	printf("Check the banlance. Press b\n");
 	printf("Deposite the amount. Press d\n");
 	printf("Withdraw the amount. Press w\n");
@@ -47,12 +67,31 @@ void menu()
 
 void check_balance(struct bank b)
 {
-	printf("Your current balance is %d\n", b.balance);
+	printf("Your current balance is %f\n", b.balance);
 }
 
-/*void deposite(struct bank b)
-{*/
-	
+void deposite(struct bank b)
+{
+	b.balance = b.balance + b.amount;
+	printf("Your current balance is: %f\n", b.balance);
+}
+
+void withdrawal(struct bank b)
+{
+	if(b.balance < b.amount) 
+	{
+		printf("You have insufficient amount\n");
+	}
+	if(b.balance > 10000)
+	{
+		printf("Daily withdrawal limit is 10000\n");
+	}
+	if(b.balance >= b.amount)	
+	{
+		b.balance = b.balance - b.amount;
+		printf("Your current balance is: %f\n", b.balance);
+	}
+}	
 
 
 
--- struct_stdinfo_fn.c
+++ struct_stdinfo_fn.c
@@ -4,6 +4,7 @@ struct student
 {
 	int roll;
 	char name[20];
+	float marks;
 };
 
 void display(struct student stu);
@@ -12,11 +13,14 @@ void main()
 {
 	struct student stu;
 
+	
 	printf("Enter name: ");
 	scanf("%s",&stu.name);
 	
-	printf("Enter roll: ");
-	scanf("%d",&stu.roll);
+	/*printf("Enter roll: ");
+	scanf("%d",&stu.roll);*/
+	stu.roll=1;
+	stu.marks=80.00;
 
 	display(stu);
 }
@@ -25,5 +29,6 @@ void display(struct student stu)
 {
 	printf("Name: %s\n",stu.name);
 	printf("Rollno: %d\n",stu.roll);
+	printf("Marks: %f\n",stu.marks);
 }
 
